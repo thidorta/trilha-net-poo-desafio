8d902b733ab6d8580cc7c3ad99a72f38a867aeae
Thiago Dorta <t231413@dac.unicamp.br>
2024-06-02 13:48:24 -0300

    Desafio concluído. Adicionado método 'Desligar'

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new("1212", "Nokia-98", "101010", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+nokia.Desligar();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new("1515", "Iphone 14", "111111110", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
+iphone.Desligar();
